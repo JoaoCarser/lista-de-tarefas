e54c05eacbd2246b0613963701573aa0d5efb08f
Joao Carser <joaogabriel-14@hotmail.com>
2023-12-18 10:15:30 -0300

    style: :art: Inicio de Estilização do .item

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,8 +7,18 @@ function TodoList(){
             <h1>Lista de Tarefas</h1>
             <form action="">
                 <input placeholder='Adicione uma tarefa' type="text" />
-                <button>Adicionar</button>
+                <button type='submit' className='add'>Adicionar</button>
             </form>
+            <div className='listaTarefas'>
+                <div className='item'>
+                    <span>Tarefa exemplo</span>
+                    <button>Deletar</button>
+                </div>
+                <div className='item completo'>
+                    <span>Tarefa exemplo</span>
+                    <button>Deletar</button>
+                </div>
+            </div>
         </div>
     )
 }
