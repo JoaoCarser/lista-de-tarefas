47645dee742ab5e65b1b3b2f311db16edfdbdf33
Joao Carser <joaogabriel-14@hotmail.com>
2023-12-21 22:25:50 -0300

    style: :art: Concluido o front do projeto, começando implementar função

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./TodoList.css"
 
-function TodoList(){
-    return(
+function TodoList() {
+
+    const [lista, setLista] = useState([]);
+    const [newItem, setNewItem] = useState("");
+
+    return (
         <div>
             <h1>Lista de Tarefas</h1>
             <form action="">
-                <input placeholder='Adicione uma tarefa' type="text" />
+                <input placeholder='Adicione uma tarefa' value={newItem} onChange={(e)=>{setNewItem(e.target.value)}} type="text" />
                 <button type='submit' className='add'>Adicionar</button>
             </form>
             <div className='listaTarefas'>
                 <div className='item'>
                     <span>Tarefa exemplo</span>
-                    <button>Deletar</button>
+                    <button className='del'>Deletar</button>
                 </div>
                 <div className='item completo'>
                     <span>Tarefa exemplo</span>
-                    <button>Deletar</button>
+                    <button className='del'>Deletar</button>
                 </div>
+                <button className='deleteAll'>Apagar todos</button>
             </div>
         </div>
     )
